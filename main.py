31cda30ed4a618d5293e7de2b6d1d93f43760fb3
Isardy <dustygirardin@gmail.com>
2018-09-11 17:08:39 +0200

    Added interactive config file generation +

--- main.py
+++ main.py
@@ -5,12 +5,30 @@
 #This program aims to make mod management for Arma 3 Dedicated Servers easier.
 #It will replace the present bash script suite and be more practical and Windows compatible.
 
+##########################################################################
+
+import os
+import configparser
+
+##########################################################################
+
+
+def servermanagement( str ):
+	if str == "stop":
+		print("stop server")
+		menu()
+	elif str == "start":
+		print("start server")
+	elif str == "update":
+		print("update server")
+	else:
+		print("Server Management error")
+
 
 ##########################################################################
 
 def menu():
-	print("Isardy's Arma 3 Dedicated Server Mod Manager.")
-	print()
+
 	print("######MAIN MENU######")
 	print()
 	print("Manager Configuration :")
@@ -29,8 +47,10 @@ def menu():
 	print("9	Check Mods for Updates")
 	print("10	Update Mods")
 	print()
+	print("0	Abort")
+	print()
 
-	choice = input('Enter your choice [1-10] : ')
+	choice = input('Enter your choice [0-10] : ')
 	choice = int(choice)
 	if choice == 1:
 		print()
@@ -63,23 +83,48 @@ def menu():
 	elif choice == 10:
 		print()
 		#Update Mods
+	elif choice == 0:
+		return
 	else:
 		print("Invalid input. Try again")
 		menu()
-
-def servermanagement( str ):
-	if str == "stop":
-		print("stop server")
-		menu()
-	elif str == "start":
-		print("start server")
-	elif str == "update":
-		print("update server")
-	else:
-		print("Server Management error")
 
 
 
 ##########################################################################
 
-menu()
+
+print("Isardy's Arma 3 Dedicated Server Mod Manager.")
+print()
+print("Reading config file...")
+
+config_file = ("./modmanager.ini")
+if not os.path.isfile(config_file):
+
+	print("Config file 'modmanager.ini' not found. Creating configuration file...")
+
+	config_file = configparser.ConfigParser()
+
+	config_file.add_section('PATHS')
+	config_file.add_section('STEAM_CREDENTIALS')
+	str = input("Enter path to steamcmd directory (exemple : /home/user/steamcmd) :")
+	config_file.set('PATHS','steamcmd', str)
+	str = input("Enter path to arma 3 server directory (exemple : /home/user/arma3) :")
+	config_file.set('PATHS','arma3sever', str)
+	str = input("Enter path where you want the mods to be installed (exemple : /home/user/arma3/mods) :")
+	config_file.set('PATHS','mods', str)
+
+	str = input("Enter your steam user name :")
+	config_file.set('STEAM_CREDENTIALS','username', str)
+	str = input("Enter your steam password :")
+	config_file.set('STEAM_CREDENTIALS','password', str)
+
+	config_file.write(open('manager.ini', 'w+'))
+
+	print("Blank configuration file created.")
+	print("Remember that you need to setup the Paths and Credentials using the following menu.")
+	print()
+	menu()
+
+else:
+	menu()
