813a5d7fe5e958f8c7d1942ac4786a819759d990
Isardy <dustygirardin@gmail.com>
2019-05-02 17:47:26 +0200

    Rewrote more functions in mods.py and config.py

--- modules/config.py
+++ modules/config.py
@@ -16,7 +16,6 @@ def generateconfigfile():
 	print("Generating config file...")
 	config_file = ("manager.ini")
 	config_file = configparser.ConfigParser(delimiters=':')
-
 	config_file.add_section('PATHS')
 	str = input("Enter path to steamcmd directory (exemple : \033[1;41m/home/user/steamcmd\033[1;m) :")
 	config_file.set('PATHS','steamcmd', str)
@@ -24,14 +23,11 @@ def generateconfigfile():
 	config_file.set('PATHS','arma3server', str)
 	str = input("Enter path, relative to the main Arma 3 directory, where you want the mods to be installed (exemple : \033[1;41mmods\033[1;m) :")
 	config_file.set('PATHS','mods', str)	
-
 	config_file.add_section('STEAM_CREDENTIALS')
 	config_file.set('STEAM_CREDENTIALS','username', str)
 	str = input("Enter your steam password :")
 	config_file.set('STEAM_CREDENTIALS','password', str)
-
 	config_file.add_section('MOD_LISTS')
-
 	config_file.write(open('manager.ini', 'w+'))
 	input("Configuration file 'manager.ini' has been created.")
 
@@ -51,7 +47,6 @@ def setconfig( option ):
 		else:
 			config_file.set('PATHS', option, new)
 			print("New ", option, " path set to \033[1;41m", new, "\033[1;m")
-
 	elif option == "username":
 		value = config_file.get('STEAM_CREDENTIALS', 'username')
 		print("Setting new Steam user name.")
@@ -62,7 +57,6 @@ def setconfig( option ):
 		else:
 			config_file.set('STEAM_CREDENTIALS','username', option)
 			print("New Steam username set to \033[1;41m", option, "\033[1;m")
-
 	elif option == "password":
 		value = config_file.get('STEAM_CREDENTIALS', 'password')
 		print("Setting new Steam password.")
@@ -72,7 +66,6 @@ def setconfig( option ):
 		else:
 			config_file.set('STEAM_CREDENTIALS','password', option)
 			print("New Steam password set.")
-
 	config_file.write(open('manager.ini', 'w'))
 
 def createlist( listname ):
@@ -83,45 +76,34 @@ def createlist( listname ):
 		config_file.add_section('WORKSHOP_MODS')
 		config_file.add_section('NON_WORKSHOP_MODS')
 		config_file.write(open(listname + '.ini', 'w+'))
-
 		config_file.read('manager.ini')
 		config_file.set('MOD_LISTS', listname, "False")
 		config_file.write(open('manager.ini', 'w+'))
-
 		return True
-
 	else:
-		
 		return False
 
 def getlists():
-
 	config_file = configparser.ConfigParser(delimiters=':')
 	config_file.read("manager.ini")
 	lists = config_file.items('MOD_LISTS')
-
 	return lists
 
 def deletelist( modlist ):
-
 	config_file = configparser.ConfigParser(delimiters=':')
 	config_file.read("manager.ini")
 	config_file.remove_option('MOD_LISTS', modlist)
 	config_file.write(open('manager.ini', 'w'))
 	os.remove(modlist + '.ini')
-
 	return True
 
 def selectlist( modlist ):
-
 	config_file = configparser.ConfigParser(delimiters=':')
 	config_file.read("manager.ini")
-
 	lists = getlists()
 	listnames = []
 	for item in lists:
 		listnames.append(item[0])
-
 	if modlist in listnames:
 		for item in lists:
 			config_file.set('MOD_LISTS', item[0], 'False')
@@ -129,4 +111,20 @@ def selectlist( modlist ):
 		config_file.write(open('manager.ini', 'w'))
 		return True
 	else :
+		return False
+
+def getpath(path):
+	config_file = configparser.ConfigParser(delimiters=':')
+	config_file.read("manager.ini")
+	if path == 'steamcmd' or path == 'arma3server' or path == 'mods':
+		return config_file.get('PATHS', path)
+	else:
+		return False
+
+def getcreds(cred):
+	config_file = configparser.ConfigParser(delimiters=':')
+	config_file.read("manager.ini")
+	if cred == 'username' or cred == 'password':
+		return config_file.get('STEAM_CREDENTIALS', cred)
+	else:
 		return False
--- modules/mods.py
+++ modules/mods.py
@@ -46,15 +46,11 @@ def addworkshopmod( profile, modid ):
 
 def addnonworkshopmod( profile, modname, directory ):
 	config_file = configparser.ConfigParser(delimiters=':')
-	config_file.read('manager.ini')
-
-	#TODO config.py funtions to get paths
-	arma3path = config_file.get('PATHS', 'arma3server')
-	modspath = config_file.get('PATHS', 'mods')
-
+	config_file.read(profile + '.ini')
+	arma3path = config.getpath('arma3server')
+	modspath = config.getpath('mods')
 	path = arma3path + '/' + modspath + '/' + directory
 	if os.path.exists(path):
-		config_file.read(profile + '.ini')
 		config_file.set('NON_WORKSHOP_MODS', directory, modname)
 		config_file.write(open(profile + '.ini', 'w'))
 		return True
@@ -67,10 +63,8 @@ def removemod( profile, ws, mod ):
 	if ws:
 		config_file.remove_option('WORKSHOP_MODS', mod)
 		config_file.write(open(profile + '.ini', 'w'))
-		config_file.read('manager.ini')
-
-		arma3path = config_file.get('PATHS', 'arma3server')
-		modspath = config_file.get('PATHS', 'mods')
+		arma3path = config.getpath('arma3server')
+		modspath = config.getpath('mods')
 		realpath = arma3path + "/" + modspath + "/steamapps/workshop/content/107410/" + mod
 		sympath = arma3path + "/" + modspath + "/" + mod
 		os.unlink(sympath)
@@ -83,10 +77,47 @@ def removemod( profile, ws, mod ):
 		shutil.rmtree(path)
 	return True
 
-def checkmodupdate( profile ):
+def modneedsupdate( profile, modid ):
 	config_file = configparser.ConfigParser(delimiters=':')
 	config_file.read(profile + '.ini')
-	for mod in config_file.items('WORKSHOP_MODS'):
-		latestupdate = getmodinfo(mod[0])[2]
+	mod = config_file.get('WORKSHOP_MODS', mod)
+	if (getmodinfo(modid)[2] == config_file.get('WORKSHOP_MODS',modid).split(',')[1]):
+		return False
+	else:
+		return True
 
+def modexists(modid):
+	arma3path = config.getpath('arma3server')
+	modspath = config.getpath('mods')
+	realpath = arma3path + "/" + modspath + "/steamapps/workshop/content/107410/" + modid
+	sympath = arma3path + "/" + modspath + "/" + modid
+	if os.path.exists(realpath) and os.path.exists(sympath):
+		return 1
+	elif os.path.exists(realpath):
+		return 2
+	else:
+		return False
 
+def downloadmod( profile, modid )
+	config_file = configparser.ConfigParser(delimiters=':')
+	config_file.read(profile + '.ini')
+	steam = config.getpath('steamcmd')
+	arma3path = config.getpath('arma3server')
+	modspath = config.getpath('mods')
+	username = config.getcreds('username')
+	password = config.getcreds('password')
+	#TODO Function to generate streamcmd commands ?
+	realpath = arma + "/" + modspath + "/steamapps/workshop/content/107410/" + modid
+	sympath = arma + "/" + modspath + "/" + modid
+	command = steam + "/steamcmd.sh " + "+login " + username + " " + password + " +force_install_dir " + arma3path + "/" + modspath + " +workshop_download_item 107410 " + modid + " +quit"
+	subprocess.call(command, shell=True, stdout=subprocess.PIPE)
+	if modexists(modid):
+		#TODO Lowerize command
+		lowerize = "find " + arma + "/" + modspath + "/" + mod + r" -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;"
+		subprocess.call(lowerize, shell=True)
+		return True
+	else:
+		os.symlink(realpath, sympath)
+		lowerize = "find " + arma + "/" + modspath + "/" + mod + r" -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;"
+		subprocess.call(lowerize, shell=True)
+		return True
